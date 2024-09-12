daa65c639a160fa6bf11cb316671882b6036a14f
edjozero <ej21serafin@gmail.com>
2024-09-11 22:09:32 -0700

    Edit function to create grid with input we want, initially 16x16

--- script.js
+++ script.js
@@ -15,8 +15,20 @@ FUNCTIONS
 */
 function starterDivs(num){
     for(let i=0; i<num; i++){
-        const div = document.createElement("div");
-        container.appendChild(div);
+        for(let j=0; j<num; j++){
+            const div = document.createElement("div");
+            div.classList.add("grid");
+            
+            container.appendChild(div);
+        }
     }
+
+    const grid = document.querySelectorAll(".grid");
+    const gridSize = Math.ceil(960/num);
+
+    grid.forEach(grid => {
+        grid.style.width = gridSize + "px";
+        grid.style.height = gridSize + "px";
+    });
 }
-starterDivs(256);
+starterDivs(8);
